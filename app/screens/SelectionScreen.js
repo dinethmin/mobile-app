731a0b76c5045824e0b5cc1ef43892383cd32427
Chanuda Dinethmin <chanudadinethmin@gmail.com>
2024-03-12 12:01:57 +0530

    code

--- app/screens/SelectionScreen.js
+++ app/screens/SelectionScreen.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import { MaterialIcons, AntDesign } from '@expo/vector-icons'
 
 function SelectionScreen(props) {
     return (
         <ImageBackground
-        style={styles.bacground}
-        source={require("../assets/SelectionScreen.png")}
-        ></ImageBackground>
+            style={styles.bacground}
+            source={require("../assets/SelectionScreen.png")}>
+
+            <View style={styles.icon}>
+                <MaterialIcons name="image-search" size={100} color="black" />
+
+                <View>
+                    <AntDesign name="camera" size={100} color="black" />
+                </View>
+
+            </View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     bacground: {
         flex: 1
+    },
+    icon: {
+        flex: 1,
+        flexDirection: 'row-reverse',
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
+        marginBottom: '10%'
     }
 })
 
