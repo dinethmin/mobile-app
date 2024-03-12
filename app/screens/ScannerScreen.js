56f54367e02490a52b88c0d637d9db82b091087f
Chanuda Dinethmin <chanudadinethmin@gmail.com>
2024-03-12 10:20:13 +0530

    code

--- app/screens/ScannerScreen.js
+++ app/screens/ScannerScreen.js
@@ -4,6 +4,7 @@ import { ImageBackground, StyleSheet } from 'react-native';
 function scannerScreen(props) {
     return (
         <ImageBackground
+        style={styles.bacground}
         source={require("../assets/ScannerScreen.png")}
         ></ImageBackground>
     );
